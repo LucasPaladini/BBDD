c8772fdda9fda1222be71c9e81983ae9a6b65d1c
lucaspaladini <lucas.paladini@alumnos.ispi4038.edu.ar>
2024-08-07 14:18:10 -0300

    probando ventanas

--- Pyside6.py
+++ Pyside6.py
@@ -1,20 +1,41 @@
-from PySide6.QtWidgets import QApplication, QLabel, QWidget
+# from PySide6.QtWidgets import QApplication, QLabel, QWidget
+# import sys
+#
+# # Crea una aplicación Qt
+# app = QApplication(sys.argv)
+#
+# # Crea un widget
+# window = QWidget()
+# window.setWindowTitle('Prueba de PySide6')
+# window.setGeometry(100, 100, 800, 500)  # x, y, ancho, alto
+#
+# # Agrega una etiqueta
+# label = QLabel('¡Hola, PySide6!', window)
+# label.setGeometry(100, 80, 100, 300)  # x, y, ancho, alto
+#
+# # Muestra el widget
+# window.show()
+#
+# # Ejecuta el ciclo de eventos
+# sys.exit(app.exec())
+
+
 import sys
+from PySide6.QtWidgets import QApplication, QWidget, QLabel
 
-# Crea una aplicación Qt
-app = QApplication(sys.argv)
+class VentanaVacia(QWidget):
 
-# Crea un widget
-window = QWidget()
-window.setWindowTitle('Prueba de PySide6')
-window.setGeometry(100, 100, 800, 500)  # x, y, ancho, alto
+    def __init__(self):
+        super().__init__()
+        self.inicializarUI()
 
-# Agrega una etiqueta
-label = QLabel('¡Hola, PySide6!', window)
-label.setGeometry(100, 80, 100, 300)  # x, y, ancho, alto
+    def inicializarUI(self):
+        self.setGeometry(100, 100, 300, 300)
+        self.setWindowTitle('Opaaa')
+        self.show()
 
-# Muestra el widget
-window.show()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ventana = VentanaVacia()
+    sys.exit(app.exec_())
 
-# Ejecuta el ciclo de eventos
-sys.exit(app.exec())
