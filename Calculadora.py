cc4c9fc3f1456fa0feb786cff35d2d02cc3c6479
lucaspaladini <lucas.paladini@alumnos.ispi4038.edu.ar>
2024-08-14 00:45:32 -0300

    Avanzado

--- Calculadora.py
+++ Calculadora.py
@@ -7,6 +7,7 @@ class MainWindow(QWidget):
         self.inicializar_ui()
 
     def inicializar_ui(self):
+        self.setFixedSize(300, 250)
         self.setGeometry(100, 100, 600, 400)
         self.setWindowTitle('Calculadora')
         self.generar_interfaz()
@@ -25,40 +26,61 @@ class MainWindow(QWidget):
         boton_8 = QPushButton("8")
         boton_9 = QPushButton("9")
         boton_0 = QPushButton("0")
-        boton_punto = QPushButton(".")
+        boton_coma = QPushButton(",")
 
         boton_suma = QPushButton("+")
         boton_resta = QPushButton("-")
         boton_multiplicacion = QPushButton("*")
         boton_division = QPushButton("/")
 
-        boton_ce = QPushButton("CE")
-        boton_borrar = QPushButton("<-")
+        boton_AC = QPushButton("AC")
+        boton_mas_menos = QPushButton("+/-")
+        boton_porciento = QPushButton("%")
         boton_resultado = QPushButton("=")
+
+        botones = [boton_1, boton_2, boton_3, boton_4, boton_5, boton_6,
+                   boton_7, boton_8, boton_9, boton_0, boton_coma, boton_suma, boton_resta,
+                   boton_multiplicacion, boton_division, boton_AC, boton_mas_menos, boton_porciento, boton_resultado]
+
+        # for boton in botones:
+        #     boton.setFixedSize(50, 50)
+
+        self.main_grid = QGridLayout()
+        self.main_grid.setHorizontalSpacing(5)  # Espaciado horizontal
+        self.main_grid.setVerticalSpacing(500)    # Espaciado vertical
+
+        operaciones = [boton_suma, boton_resta, boton_division, boton_resultado, boton_multiplicacion]
+        for boton in operaciones:
+            boton.setStyleSheet("background-color: #e89127")
+
+        funciones = [boton_AC, boton_mas_menos, boton_porciento]
+        for boton in funciones:
+            boton.setStyleSheet("background-color: #9b8a8a")
 
         self.main_grid = QGridLayout()
         self.main_grid.addWidget(self.pantalla, 0, 0, 2, 4)
-        self.main_grid.addWidget(boton_ce, 2, 0, 1, 2)
-        self.main_grid.addWidget(boton_borrar, 2, 2)
-        self.main_grid.addWidget(boton_suma, 2, 3)
+        self.main_grid.addWidget(boton_AC, 2, 0, 1, 1)
+        self.main_grid.addWidget(boton_mas_menos, 2, 1, 1, 1)
+        self.main_grid.addWidget(boton_porciento, 2, 2)
+        self.main_grid.addWidget(boton_division, 2, 3)
 
         self.main_grid.addWidget(boton_7, 3, 0)
         self.main_grid.addWidget(boton_8, 3, 1)
         self.main_grid.addWidget(boton_9, 3, 2)
-        self.main_grid.addWidget(boton_division, 3, 3)
+        self.main_grid.addWidget(boton_multiplicacion, 3, 3)
 
         self.main_grid.addWidget(boton_4, 4, 0)
         self.main_grid.addWidget(boton_5, 4, 1,)
         self.main_grid.addWidget(boton_6, 4, 2,)
-        self.main_grid.addWidget(boton_multiplicacion, 4, 3,)
+        self.main_grid.addWidget(boton_resta, 4, 3,)
 
         self.main_grid.addWidget(boton_1, 5, 0,)
         self.main_grid.addWidget(boton_2, 5, 1,)
         self.main_grid.addWidget(boton_3, 5, 2,)
-        self.main_grid.addWidget(boton_resta, 5, 3,)
+        self.main_grid.addWidget(boton_suma, 5, 3,)
 
-        self.main_grid.addWidget(boton_0, 6, 0,)
-        self.main_grid.addWidget(boton_punto, 6, 2,)
+        self.main_grid.addWidget(boton_0, 6, 0, 1, 2)
+        self.main_grid.addWidget(boton_coma, 6, 2,)
         self.main_grid.addWidget(boton_resultado, 6, 3,)
 
         self.setLayout(self.main_grid)
