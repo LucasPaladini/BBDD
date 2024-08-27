48a8d25ede795ab15a3192b117f04a76b479f514
lucaspaladini <lucas.paladini@alumnos.ispi4038.edu.ar>
2024-08-27 14:39:27 -0300

    Adelantado codigo

---
+++ QTDesigner_ui_cod.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'QtDesigner.ui'
+##
+## Created by: Qt User Interface Compiler version 6.7.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QPushButton, QSizePolicy, QTextEdit,
+    QWidget)
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        if not Form.objectName():
+            Form.setObjectName(u"Form")
+        Form.resize(400, 300)
+        self.pushButton = QPushButton(Form)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(70, 100, 51, 24))
+        self.pushButton_2 = QPushButton(Form)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+        self.pushButton_2.setGeometry(QRect(120, 100, 51, 24))
+        self.pushButton_3 = QPushButton(Form)
+        self.pushButton_3.setObjectName(u"pushButton_3")
+        self.pushButton_3.setGeometry(QRect(170, 100, 51, 24))
+        self.pushButton_4 = QPushButton(Form)
+        self.pushButton_4.setObjectName(u"pushButton_4")
+        self.pushButton_4.setGeometry(QRect(220, 100, 51, 24))
+        self.pushButton_5 = QPushButton(Form)
+        self.pushButton_5.setObjectName(u"pushButton_5")
+        self.pushButton_5.setGeometry(QRect(220, 120, 51, 24))
+        self.pushButton_6 = QPushButton(Form)
+        self.pushButton_6.setObjectName(u"pushButton_6")
+        self.pushButton_6.setGeometry(QRect(70, 120, 51, 24))
+        self.pushButton_7 = QPushButton(Form)
+        self.pushButton_7.setObjectName(u"pushButton_7")
+        self.pushButton_7.setGeometry(QRect(120, 120, 51, 24))
+        self.pushButton_8 = QPushButton(Form)
+        self.pushButton_8.setObjectName(u"pushButton_8")
+        self.pushButton_8.setGeometry(QRect(170, 120, 51, 24))
+        self.pushButton_9 = QPushButton(Form)
+        self.pushButton_9.setObjectName(u"pushButton_9")
+        self.pushButton_9.setGeometry(QRect(220, 140, 51, 24))
+        self.pushButton_10 = QPushButton(Form)
+        self.pushButton_10.setObjectName(u"pushButton_10")
+        self.pushButton_10.setGeometry(QRect(70, 160, 51, 24))
+        self.pushButton_11 = QPushButton(Form)
+        self.pushButton_11.setObjectName(u"pushButton_11")
+        self.pushButton_11.setGeometry(QRect(70, 140, 51, 24))
+        self.pushButton_12 = QPushButton(Form)
+        self.pushButton_12.setObjectName(u"pushButton_12")
+        self.pushButton_12.setGeometry(QRect(170, 160, 51, 24))
+        self.pushButton_13 = QPushButton(Form)
+        self.pushButton_13.setObjectName(u"pushButton_13")
+        self.pushButton_13.setGeometry(QRect(120, 140, 51, 24))
+        self.pushButton_14 = QPushButton(Form)
+        self.pushButton_14.setObjectName(u"pushButton_14")
+        self.pushButton_14.setGeometry(QRect(220, 160, 51, 24))
+        self.pushButton_15 = QPushButton(Form)
+        self.pushButton_15.setObjectName(u"pushButton_15")
+        self.pushButton_15.setGeometry(QRect(170, 140, 51, 24))
+        self.pushButton_16 = QPushButton(Form)
+        self.pushButton_16.setObjectName(u"pushButton_16")
+        self.pushButton_16.setGeometry(QRect(120, 160, 51, 24))
+        self.pushButton_18 = QPushButton(Form)
+        self.pushButton_18.setObjectName(u"pushButton_18")
+        self.pushButton_18.setGeometry(QRect(70, 180, 101, 24))
+        self.pushButton_19 = QPushButton(Form)
+        self.pushButton_19.setObjectName(u"pushButton_19")
+        self.pushButton_19.setGeometry(QRect(220, 180, 51, 24))
+        self.pushButton_20 = QPushButton(Form)
+        self.pushButton_20.setObjectName(u"pushButton_20")
+        self.pushButton_20.setGeometry(QRect(170, 180, 51, 24))
+        self.textEdit = QTextEdit(Form)
+        self.textEdit.setObjectName(u"textEdit")
+        self.textEdit.setGeometry(QRect(70, 60, 201, 31))
+
+        self.retranslateUi(Form)
+
+        QMetaObject.connectSlotsByName(Form)
+    # setupUi
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
+        self.pushButton.setText(QCoreApplication.translate("Form", u"AC", None))
+        self.pushButton_2.setText(QCoreApplication.translate("Form", u"+/-", None))
+        self.pushButton_3.setText(QCoreApplication.translate("Form", u"%", None))
+        self.pushButton_4.setText(QCoreApplication.translate("Form", u"/", None))
+        self.pushButton_5.setText(QCoreApplication.translate("Form", u"*", None))
+        self.pushButton_6.setText(QCoreApplication.translate("Form", u"7", None))
+        self.pushButton_7.setText(QCoreApplication.translate("Form", u"8", None))
+        self.pushButton_8.setText(QCoreApplication.translate("Form", u"9", None))
+        self.pushButton_9.setText(QCoreApplication.translate("Form", u"-", None))
+        self.pushButton_10.setText(QCoreApplication.translate("Form", u"1", None))
+        self.pushButton_11.setText(QCoreApplication.translate("Form", u"4", None))
+        self.pushButton_12.setText(QCoreApplication.translate("Form", u"3", None))
+        self.pushButton_13.setText(QCoreApplication.translate("Form", u"5", None))
+        self.pushButton_14.setText(QCoreApplication.translate("Form", u"+", None))
+        self.pushButton_15.setText(QCoreApplication.translate("Form", u"6", None))
+        self.pushButton_16.setText(QCoreApplication.translate("Form", u"2", None))
+        self.pushButton_18.setText(QCoreApplication.translate("Form", u"0", None))
+        self.pushButton_19.setText(QCoreApplication.translate("Form", u"=", None))
+        self.pushButton_20.setText(QCoreApplication.translate("Form", u",", None))
+    # retranslateUi
+
