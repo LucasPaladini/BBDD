a0f7387073f083498bf4431ff920864f13847a47
lucaspaladini <lucas.paladini@alumnos.ispi4038.edu.ar>
2024-08-28 13:19:10 -0300

    Falta todo

--- CodigoUnido.py
+++ CodigoUnido.py
@@ -1,27 +1,54 @@
-import sys
-from PySide6.QtWidgets import QApplication, QWidget
-from QTDesigner_ui_cod import Ui_Form
+from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QTextEdit, QLineEdit
+from PySide6.QtCore import Slot
+from QTDesigner_ui_cod import Ui_Form  # Asegúrate de que este nombre coincida con el archivo que contiene Ui_Form
 
-class MainWindow(QWidget, Ui_Form):
+class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)
-        self.setupConnections()
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+        self.__setup_connections()
 
-    def setupConnections(self):
-        # Conecta el botón 1 a la función handleButton1
-        self.pushButton.setText("1")
-        self.pushButton.clicked.connect(self.handleButton1)
+    def mostrar(self, texto):
+        self.ui.setText(self.ui.text() + texto)
 
-    def handleButton1(self):
-        current_text = self.textEdit.toPlainText()
-        new_text = current_text + "1"
-        self.textEdit.setPlainText(new_text)
+    def resultado(self):
+        resultado = eval(self.pantalla.text())
+        self.pantalla.setText(str(resultado))
 
-#asd
+    def __setup_connections(self):
+        # Conectar señales de botones a métodos
+        self.ui.pushButton.clicked.connect(lambda: self.mostrar("AC"), pantalla.clear())
+        self.ui.pushButton_2.clicked.connect(lambda: self.mostrar("+/-"))
+        self.ui.pushButton_3.clicked.connect(lambda: self.mostrar("%"))
+        self.ui.pushButton_4.clicked.connect(lambda: self.mostrar("/"))
+        self.ui.pushButton_5.clicked.connect(lambda: self.mostrar("*"))
+        self.ui.pushButton_6.clicked.connect(lambda: self.mostrar("7"))
+        self.ui.pushButton_7.clicked.connect(lambda: self.mostrar("8"))
+        self.ui.pushButton_8.clicked.connect(lambda: self.mostrar("9"))
+        self.ui.pushButton_9.clicked.connect(lambda: self.mostrar("-"))
+        self.ui.pushButton_10.clicked.connect(lambda: self.mostrar("1"))
+        self.ui.pushButton_11.clicked.connect(lambda: self.mostrar("4"))
+        self.ui.pushButton_12.clicked.connect(lambda: self.mostrar("3"))
+        self.ui.pushButton_13.clicked.connect(lambda: self.mostrar("5"))
+        self.ui.pushButton_14.clicked.connect(lambda: self.mostrar("+"))
+        self.ui.pushButton_15.clicked.connect(lambda: self.mostrar("6"))
+        self.ui.pushButton_16.clicked.connect(lambda: self.mostrar("2"))
+        self.ui.pushButton_17.clicked.connect(lambda: self.mostrar("0"))
+        self.ui.pushButton_18.clicked.connect(lambda: self.mostrar("="))
+        self.ui.pushButton_19.clicked.connect(lambda: self.mostrar(","))
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec())
+
+    @Slot()
+    def on_pushButton_clicked(self):
+        print("Hola")
+
+    def iniciar(self):
+        self.show()
+
+
+app = QApplication([])
+main_window = MainWindow()
+main_window.iniciar()
+
+app.exec()
